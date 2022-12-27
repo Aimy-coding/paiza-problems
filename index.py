965642cdd08a649400d16df951895ab0f6eb72d4
Aimy <aiminagasawa@gmail.com>
2022-12-27 22:26:35 +0900

    1227

--- index.py
+++ index.py
@@ -1,1 +1,13 @@
-#konkon
+#C114:しりとりの判定 12/27
+n = int(input())
+word =  [input() for i in range(n)]
+s= 0
+for i in range(n-1):
+    if word[i][-1] != word[i+1][0]:
+        print(word[i][-1] + " " +word[i+1][0])
+        break
+    else:
+       s += 1
+if s == n-1:
+    print("Yes")
+
