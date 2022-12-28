7329c9b4c3d3e5887d41221eda290ce5b8f40475
Aimy <aiminagasawa@gmail.com>
2022-12-28 19:52:18 +0900

    1228

---
+++ c016_1228.py
@@ -0,0 +1,11 @@
+
+#C016:Leet文字列
+s = str(input())
+s =s.replace("A", "4")
+s =s.replace("E", "3")
+s =s.replace("G", "6")
+s =s.replace("I", "1")
+s =s.replace("O", "0")
+s =s.replace("S", "5")
+s =s.replace("Z", "2")
+print(s)
