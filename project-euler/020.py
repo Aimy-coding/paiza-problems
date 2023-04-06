6865bdc5981a5a3e695db4b09017aece02550d25
Aimy <aiminagasawa@gmail.com>
2023-04-06 13:53:25 +0900

    done

--- project-euler/020.py
+++ project-euler/020.py
@@ -1,21 +1,21 @@
+n = 100
 
-day_month = {1: 31, 2: 28, 3:31, 4: 30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
-leap_day_month = {1: 31, 2: 29, 3:31, 4: 30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
+nResult = 1
+result = 0
 
-monthPerYear = 12
-yearsPerCentury = 100
-dayPeryear =365
-thisyear = 365
-sundayCount = 0
+for i in range(1, 100+1):
+    nResult *= i
 
-for year in range(1, yearsPerCentury+1):
-    for month in range(1, monthPerYear+1):
-        if (thisyear+1) % 7 == 0:
-            sundayCount += 1
-        if (year) %4 == 0:
-            thisyear += leap_day_month[month]
-        else: 
-            thisyear += day_month[month]
-print(sundayCount)
+nResult = str(nResult)
+ans2 = sum(map(int,str(nResult)))
 
 
+print(ans2)
+
+
+import math
+ 
+num = math.factorial(100)
+print(num)
+ans = sum(map(int,str(num)))
+print(ans)
